811f31bf22540eb6641f321b9e5cc01df86938dd
Hitesh Kumar Siwan <hiteshk888@gmail.com>
2025-02-17 23:18:40 +0530

    Added the code

---
+++ PyramidPattern/pyramidPattern.js
@@ -0,0 +1,19 @@
+function pyramidPattern(n){
+  
+    for(let i=1;i<=n;i++){
+      let str = "";
+      
+      for (let j = 1; j <= n - i; j++) {
+          str += " ";
+      }
+      
+      for(let k=1;k<=((2*i)-1);k++){
+        str+= "*";
+      }
+      
+      console.log(str)
+    }  
+  
+  }
+  
+  pyramidPattern(5);
