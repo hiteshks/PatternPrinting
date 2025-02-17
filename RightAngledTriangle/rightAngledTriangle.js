d071e6a35e059f7cff728217cc98a6d99cb17f2c
Hitesh Kumar Siwan <hiteshk888@gmail.com>
2025-02-17 22:32:59 +0530

    Added the code

---
+++ RightAngledTriangle/rightAngledTriangle.js
@@ -0,0 +1,16 @@
+function rightAngledTriangle(n){
+
+    for(let i=0;i<n;i++){
+        
+        let str  = "";
+
+        for(let j=0;j<=i;j++){
+            str += "* ";
+        }
+
+        console.log(str)
+    }
+
+}
+
+rightAngledTriangle(5)
